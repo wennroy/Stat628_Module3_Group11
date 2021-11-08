23150fd8a1649323747c3da9924a04c52294d7c0
wennroy <wennroy@gmail.com>
2021-11-08 17:39:48 -0600

    Initial Update

---
+++ code/Find_bubble_tea/find_bubble_tea.py
@@ -0,0 +1,40 @@
+import os
+import json
+import re
+
+print(os.getcwd())
+os.chdir("/")
+dataset_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset")
+save_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset_Bubble_Tea")
+
+file_name = "business.json"
+total_num = 0
+all_business_id = []
+with open(os.path.join(dataset_path, "%s" % file_name), 'r', encoding="UTF-8") as f:
+    if os.path.isfile(os.path.join(save_path, "bubble_tea_%s" % file_name)):
+        os.remove(os.path.join(save_path, "bubble_tea_%s" % file_name))
+
+    for line in f:
+        dict_json = json.loads(line)
+        categories = dict_json["categories"]
+        is_bubble_tea = False
+        if categories:
+            for category in categories.split(", "):
+                if category.lower() == "bubble tea":
+                    is_bubble_tea = True
+                    total_num += 1
+                    all_business_id.append(dict_json["business_id"])
+                    break
+
+        if is_bubble_tea:
+            with open(os.path.join(save_path, "bubble_tea_%s" % file_name), 'a') as wf:
+                wf.write(json.dumps(dict_json, indent=4))
+
+print(total_num)
+
+with open(os.path.join(save_path, "bubble_tea_business_id"), 'w') as bwf:
+    for b_id in all_business_id:
+        bwf.write(b_id+"\n")
+
+
+
---
+++ code/Preview/preview.py
@@ -0,0 +1,22 @@
+import os
+import json
+import re
+
+print(os.getcwd())
+os.chdir("/")
+dataset_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset")
+save_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset_preview")
+
+for file_name in os.listdir(dataset_path):
+    if not (re.search("json", file_name)):
+        break
+    with open(os.path.join(dataset_path, "%s" % file_name), 'r', encoding="UTF-8") as f:
+        if os.path.isfile(os.path.join(save_path, "preview_%s" % file_name)):
+            os.remove(os.path.join(save_path, "preview_%s" % file_name))
+
+        for i in range(500):
+            line = f.__next__()
+            dict_json = json.loads(line)
+
+            with open(os.path.join(save_path, "preview_%s" % file_name), 'a') as wf:
+                wf.write(json.dumps(dict_json, indent=4))

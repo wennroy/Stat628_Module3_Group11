0c25540cdf38db124e396a68c1519f945bfd5d41
wennroy <wennroy@gmail.com>
2021-11-15 21:05:23 -0600

    Wordcloud and freq bar update

---
+++ code/Wordcloud/Review_to_word_cloud_and_bar.py
@@ -0,0 +1,250 @@
+#coding:utf-8
+import os
+import json
+import numpy as np
+import time
+import datetime
+from collections import defaultdict
+
+
+os.chdir("D:\\WISC\\stat628\\Module3\\Stat628_Module3_Group11")
+print(os.getcwd())
+dataset_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset")
+save_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset_Bubble_Tea")
+review_save_path = os.path.join(os.getcwd(), "..\\yelp_dataset\\yelp_dataset_review")
+
+## Subword tokenization
+vo_1_star = defaultdict(int)
+vo_2_star = defaultdict(int)
+vo_3_star = defaultdict(int)
+vo_4_star = defaultdict(int)
+vo_5_star = defaultdict(int)
+
+for i in range(5):
+    save_file = str(i+1) + "stars.txt"
+
+    with open(os.path.join(review_save_path, "%s" % save_file), "r", encoding="UTF-8") as f:
+        line = f.__next__()
+        save_dict = json.loads(line)
+        exec("vo_%s_star = save_dict" % (i + 1))
+
+
+import wordcloud
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stopword = stopwords.words('english')
+english_punctuations_and_filter_words = [',','.',':',';','?','(',')','[',']','&','!','*','@','#','$','%','\'', '\"','/','-','\\',
+                        'get', 'got', 'back', 'ordered', 'tea','teas','milk', 'also','drink','drinks','order', 'and'
+                        'really', 'take']
+[stopword.append(_) for _ in english_punctuations_and_filter_words]
+
+# Delete the stopwords and punctuations
+
+count = [0 for _ in range(5)]
+words_count = [0 for _ in range(5)]
+for i in range(5):
+    new_dict = {}
+    exec("save_dict = vo_%s_star" % (i + 1))
+    for key in save_dict.keys():
+        count[i] += 1
+        if key in stopword:
+            continue
+        new_dict[key] = save_dict[key]
+        words_count[i] += save_dict[key]
+
+    exec("vo_%s_star = new_dict" % (i + 1))
+
+# Form a total word frequency
+total_dict = defaultdict(int)
+
+for i in range(5):
+    exec("save_dict = vo_%s_star" % (i + 1))
+    for key in save_dict.keys():
+        if key in stopword:
+            continue
+        total_dict[key] += save_dict[key]
+
+total_word = 0
+for key in total_dict.keys():
+    total_word += 1
+
+# Function of drawing a wordcloud.
+def draw_wordcloud(word_freq, pic_name, input_pic):
+    backgroud_Image = plt.imread(input_pic)
+
+
+    wc = wordcloud.WordCloud(background_color='white',# 设置背景颜色
+                             mask=backgroud_Image,
+                             max_words=2000,
+                             max_font_size=150,  # 设置字体最大值
+                             random_state=30
+                             )
+
+    wc.generate_from_frequencies(total_dict)
+    img_colors = ImageColorGenerator(backgroud_Image)
+    #字体颜色为背景图片的颜色
+    wc.recolor(color_func=img_colors)
+    plt.imshow(wc) # 显示词云
+    plt.axis('off') # 关闭坐标轴
+    plt.show() # 显示图像
+    cur_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    wc.to_file("wordcloud_pic/" + pic_name + cur_time + ".jpg")
+    print("Finished writing wordcloud %s"%pic_name)
+
+# draw_wordcloud(total_dict, "word_freq", "wordcloud.jpg")
+# draw_wordcloud(vo_1_star, "vo_1_star", "thumbs_down.jpg")
+# draw_wordcloud(vo_5_star, "vo_5_star", "thumbs_up.jpg")
+
+## tf-idf
+
+idf_vo_1_star = defaultdict(int)
+idf_vo_2_star = defaultdict(int)
+idf_vo_3_star = defaultdict(int)
+idf_vo_4_star = defaultdict(int)
+idf_vo_5_star = defaultdict(int)
+
+for i in range(5):
+    save_file = str(i+1) + "stars_idf.txt"
+
+    with open(os.path.join(review_save_path, "%s" % save_file), "r", encoding="UTF-8") as f:
+        line = f.__next__()
+        save_dict = json.loads(line)
+        exec("idf_vo_%s_star = save_dict" % (i + 1))
+
+with open(os.path.join(review_save_path, "count_sentences.txt"), "r", encoding="utf-8") as f:
+    line = f.__next__()
+    count_sentences_dict = json.loads(line)
+count_sentences = []
+for i in range(5):
+    count_sentences.append(count_sentences_dict[str(i+1)])
+
+for i in range(5):
+    new_dict = {}
+    exec("save_dict = idf_vo_%s_star" % (i + 1))
+    for key in save_dict.keys():
+        count[i] += 1
+        if key in stopword:
+            continue
+        new_dict[key] = save_dict[key]
+
+    exec("idf_vo_%s_star = new_dict" % (i + 1))
+
+
+idf_total_dict = defaultdict(int)
+for i in range(5):
+    exec("save_dict = vo_%s_star" % (i + 1))
+    for key in save_dict.keys():
+        if key in stopword:
+            continue
+        idf_total_dict[key] += save_dict[key]
+
+import math
+# Calculate the tf-idf
+# count_sentences: idf sentences count, count: word count
+
+
+tf_idf_vo_1_star = defaultdict(int)
+tf_idf_vo_2_star = defaultdict(int)
+tf_idf_vo_3_star = defaultdict(int)
+tf_idf_vo_4_star = defaultdict(int)
+tf_idf_vo_5_star = defaultdict(int)
+
+for i in range(5):
+    new_dict = {}
+    exec("save_dict = vo_%s_star" % (i + 1))
+    exec("idf_save_dict = defaultdict(int,idf_vo_%s_star)" % (i + 1))
+    for key in save_dict.keys():
+        new_dict[key] = save_dict[key]/count[i] * math.log10(sum(count_sentences)/(1+idf_save_dict[key]))
+
+    exec("tf_idf_vo_%s_star = new_dict" % (i + 1))
+
+draw_wordcloud(tf_idf_vo_1_star, "tf_idf_vo_1_star", "thumbs_down.jpg")
+draw_wordcloud(tf_idf_vo_5_star, "tf_idf_vo_5_star", "thumbs_up.jpg")
+
+for i in range(5):
+    save_file = str(i + 1) + 'stars_tf_idf.txt'
+    exec("save_dict = tf_idf_vo_%s_star" % (i + 1))
+    with open(os.path.join(review_save_path, "%s" % save_file), "w", encoding="utf-8") as wf:
+        wf.write(json.dumps(save_dict))
+
+## Draw the bar/hist
+def draw_hist_tfidf(word):
+    freq_height = [tf_idf_vo_1_star[word]]
+    freq_height.append(tf_idf_vo_2_star[word])
+    freq_height.append(tf_idf_vo_3_star[word])
+    freq_height.append(tf_idf_vo_4_star[word])
+    freq_height.append(tf_idf_vo_5_star[word])
+    print(freq_height)
+    plt.bar([1,2,3,4,5],freq_height)
+    plt.xlabel("Ratings")
+    plt.ylabel("tf-idf")
+    plt.title(word)
+
+def draw_hist_tf(word):
+    freq_height = [vo_1_star[word]]
+    freq_height.append(vo_2_star[word])
+    freq_height.append(vo_3_star[word])
+    freq_height.append(vo_4_star[word])
+    freq_height.append(vo_5_star[word])
+    print(freq_height)
+    plt.bar([1,2,3,4,5],freq_height)
+    plt.xlabel("Ratings")
+    plt.ylabel("tf")
+    plt.title(word)
+
+def draw_hist(word):
+    freq_height_tfidf = [tf_idf_vo_1_star[word]]
+    freq_height_tfidf.append(tf_idf_vo_2_star[word])
+    freq_height_tfidf.append(tf_idf_vo_3_star[word])
+    freq_height_tfidf.append(tf_idf_vo_4_star[word])
+    freq_height_tfidf.append(tf_idf_vo_5_star[word])
+    freq_height = [vo_1_star[word]/words_count[0]]
+    freq_height.append(vo_2_star[word]/words_count[1])
+    freq_height.append(vo_3_star[word]/words_count[2])
+    freq_height.append(vo_4_star[word]/words_count[3])
+    freq_height.append(vo_5_star[word]/words_count[4])
+    print(freq_height)
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    p1 = ax1.bar([1, 3, 5, 7, 9], freq_height,color='lightseagreen', label = "Term frequency",align='edge')
+    ax1.set_ylabel("Term frequency")
+    p2 = ax2.bar([2, 4, 6, 8, 10], freq_height_tfidf, label="TF-IDF",align='edge')
+    ax2.set_ylabel("TF-IDF")
+    plt.xticks([2,4,6,8,10], [1,2,3,4,5])
+    plt.tight_layout()
+    plt.legend(handles = [p1,p2])
+    plt.xlabel("Ratings")
+    plt.title(word)
+
+
+draw_hist_tfidf("music")
+
+plt.show()
+draw_hist_tf("music")
+plt.show()
+
+word_list = ['cold','hot', 'sweetness', 'ice', 'service', 'bad', 'loud', 'quiet', 'noise', 'togo', 'dessert', 'dumpling', 'affordable', 'snack',
+             'bubble','boba','comfortable','music','worse','friendly','unfriendly','friend','familiar','sugar','toppings',
+             'topping','##able','un','##ly','taro','coconut','cheap','expensive','environment','coconut','couples','delivery','food',
+             'coffee','sweet','bitter','ever','dine','ingredients','wait','time','money','never','location','delicious','app','fresh']
+word_list.sort()
+
+for word in word_list:
+    try:
+        draw_hist(word)
+        plt.savefig("wordcloud_pic/freq_pic/" + word +".jpg", dpi=600, bbox_inches = 'tight')
+    except Exception as e:
+        print(e)
+from collections import Counter
+
+most_common = Counter(total_dict).most_common()
+
+with open("most_common_words.txt", "w", encoding="UTF-8") as f:
+    for common in most_common[:300]:
+        f.write(common[0] + ': ' + str(common[1])+ "\n")
--- code/Wordcloud/Subword_tokenization.py
+++ code/Wordcloud/Subword_tokenization.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 import os
 import json
+import pathlib
 
 os.chdir("D:\\WISC\\stat628\\Module3\\Stat628_Module3_Group11")
 print(os.getcwd())
@@ -92,7 +93,7 @@ import tensorflow_text as text
 Bubble_tea_tokenizer = text.BertTokenizer('Bubble_tea_vocab.txt', **bert_tokenizer_params)
 
 # Take a look what's inside the tokenization
-
+'''
 token_sentence = []
 words = []
 sentences = []
@@ -103,6 +104,7 @@ for examples in all_data.take(10):
     words.append(Bubble_tea_tokenizer.detokenize(token_batch))
     txt_tokens.append(tf.gather(vocab, token_batch))
     sentences.append(examples[0])
+    
 
 vo_1_star = defaultdict(int)
 vo_2_star = defaultdict(int)
@@ -110,7 +112,7 @@ vo_3_star = defaultdict(int)
 vo_4_star = defaultdict(int)
 vo_5_star = defaultdict(int)
 
-## Form the frequency table for vocabulary
+## Form the frequency table for vocabulary Run about 1 hour.
 count = 0
 for examples in all_data:
     exec("save_dict = vo_%s_star" % (str(examples[1].numpy())))
@@ -123,10 +125,12 @@ for examples in all_data:
         print("Has read %s sentences." % count)
         print(txt_token)
 
+
 ## Bytes can not be written into file. Transform byte into str.
 
-new_dict = {}
+
 for i in range(5):
+    new_dict = {}
     save_file = str(i + 1) + 'stars.txt'
     exec("save_dict = vo_%s_star" % (i + 1))
     print(i)
@@ -140,14 +144,158 @@ for i in range(5):
                 print(key)
         finally:
             new_dict[new_key] = drop
-    save_dict = new_dict
+    exec("vo_%s_star_str = new_dict" % (i + 1))
 
 ## Write into files. 1stars.txt - 5stars.txt
 
 for i in range(5):
     save_file = str(i + 1) + 'stars.txt'
-    exec("save_dict = vo_%s_star" % (i + 1))
+    exec("save_dict = vo_%s_star_str" % (i + 1))
+    with open(os.path.join(review_save_path, "%s" % save_file), "w", encoding="utf-8") as wf:
+        wf.write(json.dumps(save_dict))
+'''
+# START = tf.argmax(tf.constant(reserved_tokens) == "[START]")
+# END = tf.argmax(tf.constant(reserved_tokens) == "[END]")
+# def add_start_end(ragged):
+#     count = ragged.bounding_shape()[0]
+#     starts = tf.fill([count,1], START)
+#     ends = tf.fill([count,1], END)
+#     return tf.concat([starts, ragged, ends], axis=1)
+
+'''
+## Export the tf.module
+print("Exporting the module")
+class CustomTokenizer(tf.Module):
+  def __init__(self, reserved_tokens, vocab_path):
+    self.tokenizer = text.BertTokenizer(vocab_path, lower_case=True)
+    self._reserved_tokens = reserved_tokens
+    self._vocab_path = tf.saved_model.Asset(vocab_path)
+
+    vocab = pathlib.Path(vocab_path).read_text().splitlines()
+    self.vocab = tf.Variable(vocab)
+
+    ## Create the signatures for export:
+
+    # Include a tokenize signature for a batch of strings.
+    self.tokenize.get_concrete_function(
+        tf.TensorSpec(shape=[None], dtype=tf.string))
+
+    # Include `detokenize` and `lookup` signatures for:
+    #   * `Tensors` with shapes [tokens] and [batch, tokens]
+    #   * `RaggedTensors` with shape [batch, tokens]
+    self.detokenize.get_concrete_function(
+        tf.TensorSpec(shape=[None, None], dtype=tf.int64))
+    self.detokenize.get_concrete_function(
+          tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
+
+    self.lookup.get_concrete_function(
+        tf.TensorSpec(shape=[None, None], dtype=tf.int64))
+    self.lookup.get_concrete_function(
+          tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
+
+    # These `get_*` methods take no arguments
+    self.get_vocab_size.get_concrete_function()
+    self.get_vocab_path.get_concrete_function()
+    self.get_reserved_tokens.get_concrete_function()
+
+  @tf.function
+  def tokenize(self, strings):
+    enc = self.tokenizer.tokenize(strings)
+    # Merge the `word` and `word-piece` axes.
+    enc = enc.merge_dims(-2,-1)
+    enc = add_start_end(enc)
+    return enc
+
+  @tf.function
+  def detokenize(self, tokenized):
+    words = self.tokenizer.detokenize(tokenized)
+    return cleanup_text(self._reserved_tokens, words)
+
+  @tf.function
+  def lookup(self, token_ids):
+    return tf.gather(self.vocab, token_ids)
+
+  @tf.function
+  def get_vocab_size(self):
+    return tf.shape(self.vocab)[0]
+
+  @tf.function
+  def get_vocab_path(self):
+    return self._vocab_path
+
+  @tf.function
+  def get_reserved_tokens(self):
+    return tf.constant(self._reserved_tokens)
+
+tokenizers = tf.Module()
+tokenizers.en = CustomTokenizer(reserved_tokens, 'Bubble_tea_vocab.txt')
+model_name = 'Bubble_tea'
+tf.saved_model.save(tokenizers, model_name)
+'''
+
+## Subword tokenization idf calculate
+
+
+idf_vo_1_star = defaultdict(int)
+idf_vo_2_star = defaultdict(int)
+idf_vo_3_star = defaultdict(int)
+idf_vo_4_star = defaultdict(int)
+idf_vo_5_star = defaultdict(int)
+
+## Form the frequency table for vocabulary Run about 1 hour.
+
+count_sentences = []
+[count_sentences.append(0) for _ in range(5)]
+
+print("idf frequency table")
+count = 0
+for examples in all_data:
+    temp_dict = defaultdict(int)
+    exec("save_dict = idf_vo_%s_star" % (str(examples[1].numpy())))
+    exec("count_sentences[%s] += 1" % (str(examples[1].numpy()-1)))
+    txt_token = tf.gather(vocab, Bubble_tea_tokenizer.tokenize(examples[0]).merge_dims(-2, -1))
+    count += 1
+    for words in txt_token:
+        for word in words:
+            temp_dict[word.numpy()] += 1
+    for key in temp_dict.keys():
+        save_dict[key] += 1
+
+    if count % 1000 == 0:
+        print("Has read %s sentences." % count)
+        print(txt_token)
+
+
+
+for i in range(5):
+    new_dict = {}
+    # save_file = str(i + 1) + 'stars_idf.txt'
+    exec("save_dict = idf_vo_%s_star" % (i + 1))
+    print(i)
+    for key in save_dict.keys():
+        drop = save_dict[key]
+        new_key = key
+        try:
+            new_key = str(key, encoding="utf-8")
+        except Exception as e:
+                print(e)
+                print(key)
+
+        new_dict[new_key] = drop
+
+    exec("idf_vo_%s_star_str = new_dict" % (i + 1))
+
+## Write into files. 1stars.txt - 5stars.txt
+
+for i in range(5):
+    save_file = str(i + 1) + 'stars_idf.txt'
+    exec("save_dict = idf_vo_%s_star_str" % (i + 1))
     with open(os.path.join(review_save_path, "%s" % save_file), "w", encoding="utf-8") as wf:
         wf.write(json.dumps(save_dict))
 
+count_dict = {}
+for i in range(5):
+    count_dict[i+1] = count_sentences[i]
+with open(os.path.join(review_save_path, "count_sentences.txt"), "w", encoding="utf-8") as f:
+    f.write(json.dumps(count_dict))
 
